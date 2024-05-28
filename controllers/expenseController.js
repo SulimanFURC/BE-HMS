f7a6182d56767731e24e386e6ff3052753990216
SulimanFURC <Sullaimaan@gmail.com>
2024-05-28 13:17:54 +0500

    room API completed and expense API promise handling added

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -1,36 +1,39 @@
+const asyncHandler = require("express-async-handler");
+const db = require("../config/dbConnection");
+
 //@decs Get all expenses
 //@route GET /api/expense
 //@access Public
-const getExpenses = (req, res) => {
+const getExpenses = asyncHandler(async (req, res) => {
     res.status(200).json({message: "Get All Expenses"});
-}
+})
 
 //@decs Get Single expense
 //@route GET /api/expense
 //@access Public
-const getExpense = (req, res) => {
+const getExpense = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Get Single Expense by id: ${req.params.id}`});
-}
+})
 
 //@decs Create expense
 //@route GET /api/expense
 //@access Public
-const createExpense = (req, res)=> {
+const createExpense = asyncHandler(async (req, res)=> {
     res.status(200).json({message: "Create Expense"});
-}
+})
 
 //@decs Update expense
 //@route GET /api/expense
 //@access Public
-const updateExpense = (req, res) => {
+const updateExpense = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Update Single Expense by id: ${req.params.id}`});
-}
+})
 
 //@decs Delete Expense
 //@route GET /api/expense
 //@access Public
-const deleteExpense = (req, res) => {
+const deleteExpense = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Delete Single Expense by id: ${req.params.id}`});
-}
+})
 
 module.exports = {getExpenses, getExpense, createExpense, updateExpense, deleteExpense}
--- controllers/roomController.js
+++ controllers/roomController.js
@@ -16,22 +16,53 @@ const getRooms = asyncHandler(async (req, res) => {
 //@decs Create room
 //@route GET /api/room
 //@access Public
-const createRoom = (req, res)=> {
+const createRoom = asyncHandler(async (req, res)=> {
+    const { totalCapacity, occupied, attachBath, description} = req.body;
+    try {
+        const [result] = await db.query("INSERT INTO tbl_room (totalCapacity, occupied, attachBath, description) VALUES (?, ?, ?, ?)", [totalCapacity, occupied, attachBath, description]);
+        res.status(200).json({message: "Room Created", roomID: result.insertId});
+    } catch (err) {
+        res.status(500).json({message: err})
+    }
     res.status(200).json({message: "Create Room"});
-}
+})
 
 //@decs Update Room
 //@route GET /api/room
 //@access Public
-const updateRoom = (req, res) => {
-    res.status(200).json({message: `Update Single Room by id: ${req.params.id}`});
-}
+const updateRoom = asyncHandler(async (req, res) => {
+    const roomId = req.params.id
+    const { totalCapacity, occupied, attachBath, description} = req.body
+    try {
+        const [rows] = await db.query("SELECT * FROM tbl_room WHERE roomId = ?", [roomId]);
+        if(rows.length === 0) {
+            res.status(404).json({message: "Room not found"});
+            return;
+        }
+        const [result] = await db.query("UPDATE tbl_room SET totalCapacity = ?, occupied = ?, attachBath = ?, description = ? WHERE roomId = ?", [totalCapacity, occupied, attachBath, description, roomId]);
+        res.status("200").json({message: `Room updated: ${req.params.id}`});
+    } catch(err) {
+        res.status(500).json({message: err})
+    }
+})
 
 //@decs Delete Room
 //@route GET /api/room
 //@access Public
-const deleteRoom = (req, res) => {
+const deleteRoom = asyncHandler(async (req, res) => {
+    const roomId = req.params.id;
+    try {
+        const [rows] = await db.query("SELECT * FROM tbl_room WHERE roomID = ?", [roomId]);
+        if(rows.length === 0) {
+            res.status(404).json({message: "Room not found"});
+            return;
+        }
+        await db.query("DELETE FROM tbl_room WHERE roomID = ?", [roomId]);
+        res.status(200).json({message: `Room : ${req.params.id} deleted`});
+    } catch(err) {
+        res.status(500).json({message: err});
+    }
     res.status(200).json({message: `Delete Single Room by id: ${req.params.id}`});
-}
+})
 
 module.exports = {getRooms, createRoom, updateRoom, deleteRoom}
