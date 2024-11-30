338bfa6c50bc1dd3c6dbeef1ea60b7bd48c3418b
SulimanFURC <Sullaimaan@gmail.com>
2024-11-30 18:10:32 +0500

    changes

--- Routes/dashboardRoutes.js
+++ Routes/dashboardRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const {getDashboardData} = require("../controllers/dashboardController");
 const { validateToken } = require("../middleware/validateTokenHandler");
 
+router.use(validateToken);
 // Get all Dashboard Data
-router.route("/").get(getDashboardData)
+router.route("/getDashboardData").get(getDashboardData)
 
 
 module.exports = router;
