c6840bc399189d6496fbdf51c3b7d5f62d365034
SulimanFURC <Sullaimaan@gmail.com>
2024-12-11 22:34:58 +0500

    dashboard data and dashboard chart data API implemenation

--- Routes/dashboardRoutes.js
+++ Routes/dashboardRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const router = express.Router();
-const {getDashboardData} = require("../controllers/dashboardController");
+const {getDashboardData, getMonthlyChartData} = require("../controllers/dashboardController");
 const { validateToken } = require("../middleware/validateTokenHandler");
 
 router.use(validateToken);
 // Get all Dashboard Data
 router.route("/getDashboardData").get(getDashboardData)
+router.route("/getDashboardChart").get(getMonthlyChartData)
 
 
 module.exports = router;
